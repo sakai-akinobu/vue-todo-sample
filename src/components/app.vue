c941d084a15af80f80f1e4c471476bfb43cec524
sakai-akinobu <sakai.akinobu@gmail.com>
2018-09-09 13:15:58 +0900

    Add input component.

--- src/components/app.vue
+++ src/components/app.vue
@@ -1,7 +1,33 @@
 <template>
-  <div>app</div>
+  <div>
+    <Input :value="input" :onChange="changeInput" :onEnter="addTodo" />
+  </div>
 </template>
 
 <script>
-export default {};
+import input from './input.vue';
+
+export default {
+  data() {
+    return {
+      input: '',
+      todos: [],
+    };
+  },
+  methods: {
+    changeInput(value) {
+      this.input = value;
+    },
+    addTodo(title) {
+      this.todos.push({
+        title,
+        completed: false,
+      });
+      this.input = '';
+    }
+  },
+  components: {
+    Input: input,
+  },
+};
 </script>
---
+++ src/components/input.vue
@@ -0,0 +1,21 @@
+<template>
+  <input :value="value" @input="handleInput" @keyup.enter="handleEnter" />
+</template>
+
+<script>
+export default {
+  props: {
+    value: String,
+    onChange: Function,
+    onEnter: Function,
+  },
+  methods: {
+    handleInput(e) {
+      this.onChange(e.target.value);
+    },
+    handleEnter(e) {
+      this.onEnter(e.target.value);
+    }
+  },
+};
+</script>
