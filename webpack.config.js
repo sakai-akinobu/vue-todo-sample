ad19ff3e56fa4ce2f6076e38303b15815c812c20
sakai-akinobu <sakai.akinobu@gmail.com>
2018-09-09 09:21:56 +0900

    Compile with babel.

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,10 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+    ],
+  },
   serve: {},
 };
